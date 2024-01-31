353c60b1100e4c2640ae9ad2ded57258be2fb6c3
Andy <dequin@mit.edu>
2024-01-31 18:05:26 -0500

    raw data acquired

--- strain_test/strain_test.ino
+++ strain_test/strain_test.ino
@@ -1,11 +1,35 @@
+
 int Diff = A0;  
+const int maxCount = 1000;
+static int sensorVals[maxCount];
+int Index = 0;
+
 void setup() {
-  // declare the ledPin as an OUTPUT:
   Serial.begin(9600);
-  pinMode(Diff, OUTPUT);
+  pinMode(Diff, INPUT);
+  Serial.println("begin");
+  while(!Serial);
 }
 
 void loop() {
-  Serial.println(analogRead(Diff));
-     
+  if (Index < maxCount){
+    sensorVals[Index] = analogRead(Diff);
+    delay(100);
+    Serial.print("getting data");
+    Serial.print(", ");
+    Serial.print(sensorVals[Index]);
+    Serial.print(", ");
+    Serial.println(Index);
+    Index++;
+  }
+  if (Index == maxCount){
+    Serial.println("Stored Data");
+    for(int i = 0; i<maxCount; i++){
+      Serial.println(sensorVals[i]);
+      delay(100);
+    }
+    Index++;
+  }
+  
+
 }
